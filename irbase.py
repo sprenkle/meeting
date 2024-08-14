23574ed8b345855fb85f63f3c69fda5ebc6cbd24
sprenkle <david@sprenkle.net>
2024-08-13 20:05:46 -0400

    mostly working

--- irbase.py
+++ irbase.py
@@ -19,15 +19,19 @@ def base():
     set(pins, 0)[2]
     jmp(y_dec, "loop")  # 120 cycles
 
-    nop()[29]
-    nop()[28] 
-    set(x, 5) # 60 cycles
+    set(y, 7)
+    label("start_wait")
+    nop()[31]
+    jmp(y_dec, "start_wait") # 60 cycles
 
+    set(x, 31) # 60 cycles
     label("next_bit")
-    nop()[28] 
-    in_(pins, 1) 
-    nop()[28] 
-    jmp(x_dec, "next_bit") # 60 cycles
+    nop()[27]
+    in_(pins, 1) [1]
+    nop()[31]
+    nop()[31] 
+    nop()[31] 
+    jmp(x_dec, "next_bit")[24] # 60 cycles
 
     # DONE don't worry about what is after
     push(block)
@@ -63,13 +67,13 @@ class IrBase:
 
 if __name__ == '__main__':
     
-    def invert_unsigned(x, bit_width):
-        mask = (1 << bit_width) - 1  # Create a mask with the desired bit-width
-        return ~x & mask  # Invert the bits and apply the mask
+    # def invert_unsigned(x, bit_width):
+    #     mask = (1 << bit_width) - 1  # Create a mask with the desired bit-width
+    #     return ~x & mask  # Invert the bits and apply the mask
 
 
     def handler(sm):
-        print(f'handler sm invert = {bin(sm)} or = {bin(sm ^ 0b1111_1111_1111_1111)}')
+        print(f'handler sm invert = {bin(sm)} or = {bin(sm ^ 0b1111_1111_1111_1111_1111_1111_1111_1111)}')
 
     ir_base = IrBase(handler)
     ir_base.start()
