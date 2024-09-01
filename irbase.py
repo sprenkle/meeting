438448b33a98c46826fdb680f917207f547b645c
sprenkle <david@sprenkle.net>
2024-09-01 12:27:22 -0400

    chore: Refactor irbase.py and test.py

--- irbase.py
+++ irbase.py
@@ -6,12 +6,18 @@ from ring import Ring
 from positionstate import PositionState
 import time
 
-@rp2.asm_pio(out_init=(rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW), set_init=(rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW,rp2.PIO.OUT_LOW), in_shiftdir=rp2.PIO.SHIFT_RIGHT, push_thresh=32, autopush=False)
+@rp2.asm_pio(
+    out_init=(rp2.PIO.OUT_LOW) * 8, 
+    set_init=(rp2.PIO.OUT_LOW) * 8, 
+    in_shiftdir=rp2.PIO.SHIFT_RIGHT, 
+    push_thresh=32, 
+    autopush=True,
+    fifo_join=PIO.JOIN_RX)
 def base():
     # Create start bit
     wrap_target()
     set(x, 0) # this is the number of bits to check
-   
+
 
     set(y, 20)
     label("loop")
@@ -27,14 +33,28 @@ def base():
     set(x, 31) # 60 cycles
     label("next_bit")
     nop()[27]
-    in_(pins, 1) [1]
-    nop()[31]
+    in_(pins, 8) [1]
+
+    # start of in
+    # mov(y, invert(pins))                  # 1   1
+    # jmp(not_y, "y_not_one")       # 3   3    
+    # set(y, 1)                     # 4
+    # jmp("set_output")             # 5
+
+    # label("y_not_one")                  
+    # set(y, 0)[1]                  #     4,5
+
+    # label("set_output")
+    # in_(y, 1)                     # 6    6
+    # end of in  
+
+
+    nop()[31] # was 31
     nop()[31] 
     nop()[31] 
     jmp(x_dec, "next_bit")[24] # 60 cycles
 
     # DONE don't worry about what is after
-    push(block)
     irq(0)
 
 
@@ -57,7 +77,49 @@ class IrBase:
         self.handler = handler
 
     def pin_callback(self, pin):
-        self.handler(self.sm_base.get())
+        sm1 = self.sm_base.get();
+        sm2 = self.sm_base.get();
+        sm3 = self.sm_base.get();
+        sm4 = self.sm_base.get();
+        sm5 = self.sm_base.get();
+        sm6 = self.sm_base.get();
+        sm7 = self.sm_base.get();
+        sm8 = self.sm_base.get();
+        
+        sm = int(bool((sm1 & 0b0000_0000_0000_0000_0000_0000_1111_1111))) | \
+             int(bool((sm1 & 0b0000_0000_0000_0000_1111_1111_0000_0000))) << 1 | \
+             int(bool((sm1 & 0b0000_0000_1111_1111_0000_0000_0000_0000))) << 2 | \
+             int(bool((sm1 & 0b1111_1111_0000_0000_0000_0000_0000_0000))) << 3 | \
+             int(bool((sm2 & 0b0000_0000_0000_0000_0000_0000_1111_1111))) << 4 | \
+             int(bool((sm2 & 0b0000_0000_0000_0000_1111_1111_0000_0000))) << 5 | \
+             int(bool((sm2 & 0b0000_0000_1111_1111_0000_0000_0000_0000))) << 6 | \
+             int(bool((sm2 & 0b1111_1111_0000_0000_0000_0000_0000_0000))) << 7 | \
+             int(bool((sm3 & 0b0000_0000_0000_0000_0000_0000_1111_1111))) << 8 | \
+             int(bool((sm3 & 0b0000_0000_0000_0000_1111_1111_0000_0000))) << 9 | \
+             int(bool((sm3 & 0b0000_0000_1111_1111_0000_0000_0000_0000))) << 10 | \
+             int(bool((sm3 & 0b1111_1111_0000_0000_0000_0000_0000_0000))) << 11 | \
+             int(bool((sm4 & 0b0000_0000_0000_0000_0000_0000_1111_1111))) << 12 | \
+             int(bool((sm4 & 0b0000_0000_0000_0000_1111_1111_0000_0000))) << 13 | \
+             int(bool((sm4 & 0b0000_0000_1111_1111_0000_0000_0000_0000))) << 14 | \
+             int(bool((sm4 & 0b1111_1111_0000_0000_0000_0000_0000_0000))) << 15 | \
+             int(bool((sm5 & 0b0000_0000_0000_0000_0000_0000_1111_1111))) << 16 | \
+             int(bool((sm5 & 0b0000_0000_0000_0000_1111_1111_0000_0000))) << 17 | \
+             int(bool((sm5 & 0b0000_0000_1111_1111_0000_0000_0000_0000))) << 18 | \
+             int(bool((sm5 & 0b1111_1111_0000_0000_0000_0000_0000_0000))) << 19 | \
+             int(bool((sm6 & 0b0000_0000_0000_0000_0000_0000_1111_1111))) << 20 | \
+             int(bool((sm6 & 0b0000_0000_0000_0000_1111_1111_0000_0000))) << 21 | \
+             int(bool((sm6 & 0b0000_0000_1111_1111_0000_0000_0000_0000))) << 22 | \
+             int(bool((sm6 & 0b1111_1111_0000_0000_0000_0000_0000_0000))) << 23 | \
+             int(bool((sm7 & 0b0000_0000_0000_0000_0000_0000_1111_1111))) << 24 | \
+             int(bool((sm7 & 0b0000_0000_0000_0000_1111_1111_0000_0000))) << 25 | \
+             int(bool((sm7 & 0b0000_0000_1111_1111_0000_0000_0000_0000))) << 26 | \
+             int(bool((sm7 & 0b1111_1111_0000_0000_0000_0000_0000_0000))) << 27 | \
+             int(bool((sm8 & 0b0000_0000_0000_0000_0000_0000_1111_1111))) << 28 | \
+             int(bool((sm8 & 0b0000_0000_0000_0000_1111_1111_0000_0000))) << 29 | \
+             int(bool((sm8 & 0b0000_0000_1111_1111_0000_0000_0000_0000))) << 30 | \
+             int(bool((sm8 & 0b1111_1111_0000_0000_0000_0000_0000_0000))) << 31 
+
+        self.handler(sm);
         
     def start(self):
         self.sm_base.active(True)
